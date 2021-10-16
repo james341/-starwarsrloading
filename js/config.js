ef14dcd2bcb392c20cb548934ae87620d628137f
james341 <taine049@gmail.com>
2021-10-16 18:43:08 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -129,9 +129,9 @@ var l_musicDisplay = true;
 // (Place the ogg files in the music folder)
 // (You can also use external URL for the ogg files)
 var l_musicPlaylist = [
-	{ogg: "viccecitymall.ogg", name: "Nations - Installation Music"},
-	{ogg: "starwarsmaintheme.ogg", name: "Nations - Installation Music"},
-	{ogg: "letitfrozenparody.ogg", name: "Nations - Installation Music"},
+	{ogg: "viccecitymall.ogg", name: "Neutron - Installation Music"},
+	{ogg: "starwarsmaintheme.ogg", name: "Neutron - Installation Music"},
+	{ogg: "letitfrozenparody.ogg", name: "Neutron - Installation Music"},
 ];
 
 // Random music order?
